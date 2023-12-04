8e4260788f67ae7cfd7aef892bd11e5d4b391c0d
Diego Olivieri <diegoolivierivii@gmail.com>
2023-12-04 18:01:28 -0300

    refactor: executeScheduledDataProcessor instead of getData. getData method was not doing what the task was. executeScheduledDataProcessor method does the service invocation as expected.

--- force-app/main/default/lwc/restCalloutButton/restCalloutButton.js
+++ force-app/main/default/lwc/restCalloutButton/restCalloutButton.js
@@ -1,13 +1,13 @@
 import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import getData from '@salesforce/apex/Ucl_RestCallout.getData';
+import executeScheduledDataProcessor from '@salesforce/apex/Ucl_ScheduledDataProcessor.executeScheduledDataProcessor';
 
 export default class RestCalloutButton extends LightningElement {
     async invokeService() {
         try {
-            const result = await getData();
             
-            console.log('Service Result:', result);
+            await executeScheduledDataProcessor();
+
             const toastEvent = new ShowToastEvent({
                 title: 'Service Invocation',
                 message: 'Service invoked successfully!',
